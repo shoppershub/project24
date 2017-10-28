f49c49d3b63f251baff10282afbe0e1829a3180c
maulikchevli <maulikchevli@gmail.com>
2017-10-28 19:33:06 +0530

    Add DATABASE support in login,register,and index

---
+++ products.sql
@@ -0,0 +1,2 @@
+CREATE TABLE products(name TEXT NOT NULL, brand TEXT NOT NULL, color TEXT NOT NULL, storage TEXT NOT NULL, price NUMBER NOT NULL);
+CREATE TABLE user(name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL);
--- project.py
+++ project.py
@@ -1,21 +1,90 @@
-from flask import Flask,render_template
+import sqlite3 as sql
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # TODO - Add Images in database
+    con = sql.connect('products.db')
+    con.row_factory = sql.Row
 
-@app.route('/login')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM products')
+
+    products = cur.fetchall()
+    con.close()
+    return render_template('index.html', products = products)
+
+''' Allows user to login, nothing else for now '''
+@app.route('/login',methods = ['POST','GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        try:
+            enteredPassword = request.form['password']
+            userEmail = request.form['email']
 
-@app.route('/register')
+            con = sql.connect('products.db')
+            con.row_factory = sql.Row
+
+            cur = con.cursor()
+            ret = cur.execute('SELECT password FROM user WHERE email=?',(userEmail,))
+            storedPassword = ret.fetchone()[0] #Might throw error
+
+            con.close()
+            if enteredPassword == storedPassword:
+                return render_template('welcome_user.html',msg = "Welcome back " + userEmail, a_user = True)
+            else:
+                return render_template('welcome_user.html',msg = "New User?", a_user = False)
+        except TypeError:
+            return render_template('welcome_user.html',msg = "New User?", a_user = False)
+
+@app.route('/register', methods = ['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    else:
+        try:
+            name = request.form['name']
+            email = request.form['email']
+            password = request.form['password']
+            confirmPassword = request.form['confirmPassword']
+
+            # TODO - Values should not be null
+            # TODO - validate if passwords are same
+            # TODO - Hash the password
+
+            with sql.connect("products.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO user (name,email,password) VALUES (?,?,?)",(name,email,password))
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "Error in insert operation"
+
+        finally:
+            con.close()
+            return render_template('result.html', msg = msg)
+            #TODO - Differentiate Admins and user by radiobutton
 
 @app.route('/customerCare')
 def customerCare():
     return render_template('customerCare.html')
+
+''' Only for admins '''
+@app.route('/listUsers')
+def listUsers():
+    con = sql.connect('products.db')
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute('SELECT * FROM user')
+
+    rows = cur.fetchall()
+    return render_template('listUsers.html', rows = rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
